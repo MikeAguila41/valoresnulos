0ca0b1acfa254f85d3df3414f3f8ec5959092586
Miguel aguila <A01706165@tec.mx>
2024-04-19 00:09:54 -0600

    clientes,py , devoluciones. py

---
+++ devoluciones.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+#Carga desde un archivo .xlsx sin indice
+df = pd.read_excel("devoluciones.xlsx")
+
+#print(df.head())
+print(df.info())
+#print(df.describe())
+print(df.isnull().sum())
+
+#Quitar nulos
+
+#Para RFC voy a sustituir el valor nulo con un cero, ya que no es una columna numérica y no podemos aplicar algún método
+#df['RFC'] = df['RFC'].fillna(0)
+#valores_nulos = df.isnull().sum()
+
+#Para NOMBRE también voy a sustituir el valor nulo con un cero, ya que no es una columna numérica y no podemos aplicar algún método
+#df['NOMBRE'] = df['NOMBRE'].fillna(0)
+#valores_nulos = df.isnull().sum()
+
+#print(valores_nulos)
